bdd3b8eaa170aad1059af8505f8f135d83674fe4
Vkscoma <vkscoma@gmail.com>
2023-12-08 10:45:44 -0800

    Updating bankAccount test

--- Bank Account Test/bankAccount.js
+++ Bank Account Test/bankAccount.js
@@ -4,16 +4,26 @@
 
 class BankAccount {
     constructor(balance = 0, accountHolder, accountNumber) {
+        if (typeof balance !== "number") {
+            throw new Error("Balance must be a number");
+        }
         this.balance = balance;
         this.accountHolder = accountHolder;
         this.accountNumber = accountNumber;
     }
     depositToAccount(amt) {
-        return this.balance += amt
+        if (amt <= 0) {
+            throw new Error("Deposit amount must be positive");
+        } else {
+            const newAccountValue = this.balance += amt
+            console.log(`Your new balance is $${newAccountValue}`);
+        }
     }
     withdrawFromAccount(amt) {
-        if (amt <= this.balance)
-            return this.balance -= amt
+        if (amt <= this.balance) {
+            const newBalance = this.balance -= amt
+            console.log(`Your new balance is $${newBalance}`)
+        }
         else {
             throw new Error("Insufficient funds");
         }
